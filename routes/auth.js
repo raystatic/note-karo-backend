3f18df514829bb9d28ee08ef769aac7c6c380652
raystatic <rahul9650ray@gmail.com>
2020-10-06 18:20:21 +0530

    project setup and auth routes created

---
+++ routes/auth.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+router.use(bodyParser.json());
+
+router.post('/auth', (req, res) => {
+
+    User.findOne({email:req.body.email})
+        .then((user) => {
+            if(user === null){
+                User.create(req.body)
+                    .then((user) => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-type','application/json');
+                        const token = jwt.sign({_id:user._id},process.env.TOKEN_SECRET);
+                        res.header('auth-token',token)
+                        res.json({
+                            _token: token,
+                            _user:user
+                        });
+                    }, (err) => {
+                        res.statusCode = 400;
+                        res.json({
+                            error:err
+                        });
+                    })
+                    .catch((err) => {
+                        res.statusCode = 400;
+                        res.json({
+                            error:err
+                        });
+                    })
+            }else{
+                res.statusCode = 200;
+                res.setHeader('Content-type','application/json');
+                const token = jwt.sign({_id:user._id},process.env.TOKEN_SECRET);
+                res.header('auth-token',token)
+                res.json({
+                    _token: token,
+                    _user:user
+                });
+            }
+        }, (err) => {
+            res.statusCode = 400;
+            res.json({
+                error:err
+            });
+        })
+        .catch((err) => {
+            res.statusCode = 400;
+            res.json({
+                error:err
+            });
+        })    
+
+})
+
+module.exports = router;

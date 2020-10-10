8d7795c574f4bf4af413728f6e6928cc12a58f7e
raystatic <rahul9650ray@gmail.com>
2020-10-10 08:17:50 +0530

    auth.js updated

--- routes/auth.js
+++ routes/auth.js
@@ -7,17 +7,6 @@ router.use(bodyParser.json());
 
 router.post('/auth', (req, res) => {
 
-    console.log('auth-debug: enter',req.body);
-
-    User.find((err, data) => {
-        if(data){
-            console.log('all users data', data);
-        }else{
-            console.log('all users err', err);
-        }
-        
-    })
-
     User.findOne({email:req.body.email},(err, user) => {
 
         console.log('auth-debug: fetched user',user);
